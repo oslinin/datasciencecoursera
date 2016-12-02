78c2e92a5e3c0873d20b44cdd198ce52e4e52ac3
oslinin <oleg.slinin@gmail.com>
2016-12-02 15:53:01 -0500

    Update TxHandler.java

--- bitcoin/assignment1starterCode/TxHandler.java
+++ bitcoin/assignment1starterCode/TxHandler.java
@@ -1,53 +1,122 @@
 import java.security.PublicKey;
+import java.security.Signature;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+ 
 
 public class TxHandler {
-UTXOPool utxoPoolL;
+	
+	UTXOPool utxoPoolL;
+	
     public TxHandler(UTXOPool utxoPool) {
         utxoPoolL = new UTXOPool(utxoPool);
       }
 
     public boolean isValidTx(Transaction tx) {
-boolean ok1 = true;
+    	//boolean ok1 = true;
         ArrayList<UTXO> inputslist = new ArrayList<UTXO>();
         double intot=0;
     	for (int i = 0 ; i < tx.numInputs() ; i++) { //iterate inputs
     		byte[] rawdata = tx.getRawDataToSign(i);
     		Transaction.Input ti = tx.getInput(i);
     		UTXO txo = new UTXO(ti.prevTxHash, ti.outputIndex);
-    		ok1 = ok1 & 
+    		inputslist.add(txo);
+    		//ok1 = ok1 & 
     		        // (1) all outputs claimed by code tx are in the current UTXO pool,
-    				utxoPoolL.contains(txo) &
-    				// (2) the signatures on each input of code tx are valid,		
-    				Crypto.verifySignature(utxoPoolL.getTxOutput(txo).address, rawdata , ti.signature);    		
+    		if (!utxoPoolL.contains(txo)) return false; 
+    				// (2) the signatures on each input of code tx are valid,
+    		
+    			//ok1=ok1&	Crypto.verifySignature(utxoPoolL.getTxOutput(txo).address, rawdata , ti.signature);
+    		try{
+    			Transaction.Output o = utxoPoolL.getTxOutput(txo);     	
+    			RSAKey k = o.address;
+    			byte[] sig = tx.getInput(i).signature;
+    			boolean p = k.verifySignature(tx.getRawDataToSign(i) , sig); 
+    			if (!p) return false;
+    		} catch (Exception e) {
+    			return false;
+    		}
+    		
     		
                 try {
-                    intot += utxoPoolL.getTxOutput(txo).value;
+                	Transaction.Output x = utxoPoolL.getTxOutput(txo);
+                    intot += x.value;
                 } catch(NullPointerException e)    {    
                     return false;
-                                }
-    		inputslist.add(txo);
+                } catch(Exception e2){
+                	return false;
+                }
+    		
      	}
     	
     	Collections.sort(inputslist);
-    	for (int i = 2; i < inputslist.size(); i++) // (3) no UTXO is claimed multiple times by code tx,
-    		if (inputslist.get(i).equals(inputslist.get(i-1))) ok1=false;
+    	for (int i = 1; i < inputslist.size(); i++) // (3) no UTXO is claimed multiple times by code tx,
+    		if (inputslist.get(i).equals(inputslist.get(i-1))) return false;
     	
     	double outtot=0;
        	for (int i = 0 ; i < tx.numOutputs() ; i++) { //iterate outputs
-       		if (tx.getOutput(i).value<0) ok1=false; // (4)  all of code txs output values are non-negative
+       		if (tx.getOutput(i).value<0) return false; // (4)  all of code txs output values are non-negative
        		outtot += tx.getOutput(i).value;
        	}
-       	if (outtot>intot) ok1=false; //(5) the sum of code txs input values is greater than or equal to the sum of its output values; and false otherwise.
+       	if (outtot>intot) return false; //(5) the sum of code txs input values is greater than or equal to the sum of its output values; and false otherwise.
        	
-    	return ok1;    }
-
-    public Transaction[] handleTxs(Transaction[] possibleTxs) {
-ArrayList<Transaction> trs = new ArrayList<Transaction>(Arrays.asList(possibleTxs));
+    	return true;    }
+    public boolean isValidTx2(Transaction tx) {
+    	//boolean ok1 = true;
+        ArrayList<UTXO> inputslist = new ArrayList<UTXO>();
+        double intot=0;
+    	for (int i = 0 ; i < tx.numInputs() ; i++) { //iterate inputs
+    		byte[] rawdata = tx.getRawDataToSign(i);
+    		Transaction.Input ti = tx.getInput(i);
+    		UTXO txo = new UTXO(ti.prevTxHash, ti.outputIndex);
+    		inputslist.add(txo);
+    		//ok1 = ok1 & 
+    		        // (1) all outputs claimed by code tx are in the current UTXO pool,
+    		if (!utxoPoolL.contains(txo)) return false; 
+    				// (2) the signatures on each input of code tx are valid,
+    		
+    			//ok1=ok1&	Crypto.verifySignature(utxoPoolL.getTxOutput(txo).address, rawdata , ti.signature);
+    		try{
+    			Transaction.Output o = utxoPoolL.getTxOutput(txo);     	
+    			RSAKey k = o.address;
+    			byte[] sig = tx.getInput(i).signature;
+    			boolean p = k.verifySignature(tx.getRawDataToSign(i) , sig); 
+    		//	if (!p) return false;
+    		} catch (Exception e) {
+    			return false;
+    		}
+    		
+    		
+                try {
+                	Transaction.Output x = utxoPoolL.getTxOutput(txo);
+                    intot += x.value;
+                } catch(NullPointerException e)    {    
+                    return false;
+                } catch(Exception e2){
+                	return false;
+                }
+    		
+     	}
     	
+    	Collections.sort(inputslist);
+    	for (int i = 1; i < inputslist.size(); i++) // (3) no UTXO is claimed multiple times by code tx,
+    		if (inputslist.get(i).equals(inputslist.get(i-1))) return false;
+    	
+    	double outtot=0;
+       	for (int i = 0 ; i < tx.numOutputs() ; i++) { //iterate outputs
+       		if (tx.getOutput(i).value<0) return false; // (4)  all of code txs output values are non-negative
+       		outtot += tx.getOutput(i).value;
+       	}
+       	if (outtot>intot) return false; //(5) the sum of code txs input values is greater than or equal to the sum of its output values; and false otherwise.
+       	
+    	return true;    }
+    public Transaction[] handleTxs(Transaction[] possibleTxs) {
+    	ArrayList<Transaction> trs = new ArrayList<Transaction>(Arrays.asList(possibleTxs));
+    	ArrayList<Transaction> trs2 = new ArrayList<Transaction>();
+		
+
     	//trs.forEach((trx) -> {if (!isValidTx(trx)) trs.remove(trx);});
     	//for (Transaction tx1 : trs) if (!isValidTx(tx1)) trs.remove(tx1);
     	Transaction txr2 = new Transaction();
@@ -55,18 +124,25 @@ ArrayList<Transaction> trs = new ArrayList<Transaction>(Arrays.asList(possibleTx
     		if (isValidTx(tx1)) { // remove invalid transactions.
     			
     			//try adding tx1 to txr2; call it txr
-    			Transaction txr = new Transaction(txr2);
-    			for (Transaction.Input  ti : tx1.getInputs())  txr.addInput( ti.prevTxHash, ti.outputIndex);
-    			for (Transaction.Output ti : tx1.getOutputs()) txr.addOutput(ti.value, ti.address);
+    			Transaction txr = txr2;
+    			for (Transaction.Input  ti : tx1.getInputs())  {
+    				txr.addInput( ti.prevTxHash, ti.outputIndex);
+    				txr.addSignature(ti.signature, txr.numInputs()-1);
+    			}
+    			for (Transaction.Output ti : tx1.getOutputs()){
+    				txr.addOutput(ti.value, ti.address);
+    			}
     			txr.finalize();
     			
-    			if (isValidTx(txr)) {
+    			if (isValidTx2(txr)) {
     				txr2=txr;
-    				break;
-    			} 
-    		} // end if (isValidTx(tx1)) {
-    		trs.remove(tx1); // remove invalid transactions and those that mess up txr2 running transaction array.
+    				trs2.add(tx1);
+    			}  
+    		} //else {trs.remove(tx1);} // remove invalid transactions and those that mess up txr2 running transaction array.
     	}
-        return  (Transaction[])trs.toArray();    }
+    	Transaction[] x = new Transaction[trs2.size()];
+    	for(int i =0; i<trs2.size(); i++) x[i]=trs2.get(i);
+        return  x;
 
+    }
 }
