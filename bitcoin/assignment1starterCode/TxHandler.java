128051aab5270bf393b78f4ed23207b2cc2dfded
oslinin <oleg.slinin@gmail.com>
2016-12-04 23:21:54 -0500

    Update TxHandler.java

--- bitcoin/assignment1starterCode/TxHandler.java
+++ bitcoin/assignment1starterCode/TxHandler.java
@@ -1,20 +1,18 @@
 import java.security.PublicKey;
-import java.util.ArrayList;
+import java.security.Signature;
+import java.util.ArrayList;a
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-public class MaxFeeTxHandler {
-    public MaxFeeTxHandler(UTXOPool utxoPool) {
-    	utxoPoolL = new UTXOPool(utxoPool);
-    }
+ 
 
-    /**@return true if:
-     * (1) all outputs claimed by code tx are in the current UTXO pool, 
-     * (2) the signatures on each input of code tx are valid, 
-     * (3) no UTXO is claimed multiple times by code tx,
-     * (4) all of code txs output values are non-negative, and
-     * (5) the sum of code txs input values is greater than or equal to the sum of its output
-     *     values; and false otherwise.     */
+public class TxHandler {
+	
+	
+    public TxHandler(UTXOPool utxoPool) {
+        utxoPoolL = new UTXOPool(utxoPool);
+      }
+
     public boolean isValidTx(Transaction tx) {
     	//boolean ok1 = true;
         ArrayList<UTXO> inputslist = new ArrayList<UTXO>();
@@ -112,123 +110,48 @@ public class MaxFeeTxHandler {
        	}
        	if (outtot>intot) return false; //(5) the sum of code txs input values is greater than or equal to the sum of its output values; and false otherwise.
        	
-    	return true;    }
-     /**
-     * Handles each epoch by receiving an unordered array of proposed transactions, checking each
-     * transaction for correctness, returning a mutually valid array of accepted transactions, and
-     * updating the current UTXO pool as appropriate.
-     */
-    public class Transaction2{
-    	public Transaction t;
-    	public ArrayList<Integer> i;    
-    	public Transaction2(Transaction x, ArrayList<Integer> j){t=x; i=j;}
-    	public Transaction2(Transaction x,int j){t=x; i=new ArrayList<Integer>(0); i.add(j);}
-    	public void bindTx2(Transaction x, int j){t = bindTx(t, x); i.add(j);}    	  
-    }
+    	return true;    
+    	}
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
-    	//ArrayList<Transaction> trs = new ArrayList<Transaction>(Arrays.asList(possibleTxs));
-    	//for (Transaction tx1 : trs){if (!isValidTx(tx1)) trs.remove(tx1);}
-    	ArrayList<Transaction> trs = new ArrayList<Transaction>();
-    	for (int k = 0 ; k<possibleTxs.length; k++) 
-    		if (isValidTx(possibleTxs[k])) 
-    			trs.add(possibleTxs[k]);
-    	
-    	double x = totalValue(trs);
-    	int tot = (int)Math.ceil(x*1000);
-    	int[][] K = new int[trs.size()][tot];   	
-    	Transaction2[][] T = new Transaction2[trs.size()][tot];
-    	//Knapsack where weight = value.
-    	int a=0,b=0,v=0,i=0,j=0;
-    	v = (int)totalValue(trs.get(0));
-    	for (j = 0; j< tot;  j++) {
-    		if (j>=v) {
-    			K[0][j]=v; 
-    			T[0][j]=new Transaction2(trs.get(0), 0);
-    		}
-    	}
-    	
-    	
-    	for (i = 1; i< trs.size();  i++){ //transactions = x
-    		v = (int)totalValue(trs.get(i));
-    		for (j = 0; j<tot; j++){ //volumes = y   			
-    			a = K[i-1][j];
-    			//b = K[i-1][j-v] + v;
-    			if (j<v){
-    				K[i][j]=a; T[i][j]=T[i-1][j];
-    			} else {
-        			Transaction2 n =T[i-1][j-v];
-        			n.bindTx2(trs.get(i),i);    			
-        			if (!isValidTx2(n.t)) {
-        				b=0;
-        			} else {
-        				b=totalValue(n.t);
-        			}
-        			if (b>a){ 
-        				K[i][j]=b; T[i][j]=n;
-        			} else {
-    					K[i][j]=a; T[i][j]=T[i-1][j];
-    				}
+    	ArrayList<Transaction> trs = new ArrayList<Transaction>(Arrays.asList(possibleTxs));
+    	ArrayList<Transaction> trs2 = new ArrayList<Transaction>();
+		
+
+    	//trs.forEach((trx) -> {if (!isValidTx(trx)) trs.remove(trx);});
+    	//for (Transaction tx1 : trs) if (!isValidTx(tx1)) trs.remove(tx1);
+    	Transaction txr2 = new Transaction();
+    	for (Transaction tx1 : trs){ 
+    		if (isValidTx(tx1)) { // remove invalid transactions.
+    			
+    			//try adding tx1 to txr2; call it txr
+    			Transaction txr = txr2;
+    			for (Transaction.Input  ti : tx1.getInputs())  {
+    				txr.addInput( ti.prevTxHash, ti.outputIndex);
+    				txr.addSignature(ti.signature, txr.numInputs()-1);
     			}
-    		}
+    			for (Transaction.Output ti : tx1.getOutputs()){
+    				txr.addOutput(ti.value, ti.address);
+    			}
+    			txr.finalize();
+    			
+    			if (isValidTx2(txr)) {
+    				txr2=txr;
+    				trs2.add(tx1);
+    			}  
+    		} //else {trs.remove(tx1);} // remove invalid transactions and those that mess up txr2 running transaction array.
     	}
-	int best=0;
-	for (i=0; i<trs.size(); i++)
-		for (j=0; j<tot; j++)
-			if(K[i][j]>best) {
-				best=K[i][j];a=i; b=j;
-			}
-        
-    	/*Transaction[] res=new Transaction[T[a][b].i.size()];
-    	for( int k = 0 ; k < res.length; k++) res[k]=trs.get(k);
-    	return res;*/
-    	
-    	Transaction[] res = new Transaction[T[a][b].i.size()];
-    	for(int i1 =0; i1< res.length; i1++) {
-    		Transaction y = trs.get(i1);
-    		res[i1] = y;
-    		for (int j1 =0; j1<y.numInputs(); j1++){
-        		Transaction.Input ti = y.getInput(j1);
+    	Transaction[] x = new Transaction[trs2.size()];
+    	for(int i =0; i<trs2.size(); i++) {
+    		Transaction y = trs2.get(i);
+    		x[i] = y;
+    		for (int j =0; j<y.numInputs(); j++){
+        		Transaction.Input ti = y.getInput(j);
     			UTXO txo = new UTXO(ti.prevTxHash, ti.outputIndex);
     			utxoPoolL.removeUTXO(txo);
     		}
     	}
-    	return  res;
+    	return  x;
     }
+	UTXOPool utxoPoolL;
 
-
- 
-    public int totalValue(Transaction tx){
-    	double outtot=0;     	
-       		for (int j = 0; j<tx.numOutputs(); j++){
-       			outtot += tx.getOutput(j).value;
-       		}       	
-       	return (int)outtot;
-    }
-    public double totalValue(ArrayList<Transaction> trs){
-    	//return totalValue((Transaction[])trs.toArray());  
-    	Transaction[] x = new Transaction[trs.size()];
-    	for (int i =0; i<x.length; i++) x[i]=trs.get(i);
-    	return totalValue(x);
-    }
-    public double totalValue(Transaction[] tx){
-    	double outtot=0;
-       	for (int i = 0 ; i < tx.length ; i++) { //iterate outputs
-       		for (int j = 0; j<tx[i].numOutputs(); j++){
-       			outtot += tx[i].getOutput(j).value;
-       		}
-       	}
-       	return outtot;
-    }
-	public Transaction bindTx(Transaction txr2, Transaction tx1){
-		if (txr2==null & tx1==null) return null;
-		if (txr2==null) return tx1;
-		if (tx1==null) return txr2;
-		Transaction txr = new Transaction(txr2);
-		for (Transaction.Input  ti : tx1.getInputs())  txr.addInput( ti.prevTxHash, ti.outputIndex);
-		for (Transaction.Output ti : tx1.getOutputs()) txr.addOutput(ti.value, ti.address);
-		txr.finalize();
-		return txr;
-	}
-	
-    UTXOPool utxoPoolL;
 }
